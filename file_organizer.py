087a1362e0e11b202583da559f2ab6955252602f
Vincent Bui <vcbui1@uno.edu>
2026-01-10 20:59:13 -0600

    Added user input and path validation

--- file_organizer.py
+++ file_organizer.py
@@ -1,57 +1,56 @@
-#Separate Files from Folders
 import os
+import shutil
+
+#Ask user to enter folder path
+folder_path = input("Enter the folder's path: ")
+
+#Check if the folder path is correct 
+if not os.path.exists(folder_path):
+    print("Folder not found! Please check the path.")
+    exit()
+
+#Check if it's a folder
+if not os.path.isdir(folder_path):
+    print("That's not a folder!")
+    exit()
 
 #Returns a list of names in a directory
-files = os.listdir()
+files = os.listdir(folder_path)
 
-#loop through each file to check if its a file
-for file in files:
-    if file == "file_organizer.py":
-        continue  # skip this file
-    
-    #get full path of the selected folder
-    file_path = os.path.join("../python_projects", file)
-    if os.path.isfile(file_path):
-        #Extract File Extensions
-        extension = os.path.splitext(file)[-1]
-        print(f"File: {file}, Extension: {extension}")
-
-#Group Files by Type
+#Group Files by Type 
 file_groups = {}  # empty dictionary
+
 for file in files:
     #Exclude your own script from being organized
     if file == "file_organizer.py":
         continue  
 
-    #Combine the folder path with the filename
-    file_path = os.path.join("../python_projects", file)
+    #Build path from current directory
+    file_path = os.path.join(folder_path, file)
+
+    #Check if its a file
     if os.path.isfile(file_path):
         #Get the extension by separating the file from extension
         extension = os.path.splitext(file)[-1]
+        
+        #If key doesn't exist, add new content to dictionary 
         if extension not in file_groups:
             file_groups[extension] = [file]
+        #If key exist, add values to the list inside the dictionary
         else:
             file_groups[extension].append(file)
 
-#Create Folders and Move Files
-import shutil
-
-# After you've built your file_groups dictionary:
 for extension, file_list in file_groups.items():
-    # Create a folder for this extension (handle if it exists)
-    folder_name = extension[1:] + "_files"
+    #Build path from current directory
+    folder_name = os.path.join(folder_path, extension[1:] + "_files")
+
+    #Create a folder for this extension (handle if it exists)
     os.makedirs(folder_name, exist_ok=True)
-    # Move file to the folder
+    
+    #Move each file to the folder based on extension
     for file in file_list:
-        shutil.move(file, folder_name)
+        source = os.path.join(folder_path, file)
+        shutil.move(source, folder_name)
 
-print("✅ Files organized successfully!")
-
-
-
-
-
-
-
-
-
+#End of program
+print("\nFiles organized successfully!")
